ff602ef954dc66a3a6e0e6ae71cb1656977d921a
zekedellavechia <67669609+zekedellavechia@users.noreply.github.com>
2021-01-18 18:46:13 -0300

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -20,7 +20,7 @@ import './commands'
 // require('./commands')
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
